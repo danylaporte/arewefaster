480889d36822077d9797c38fad49a2583cb97279
danylaporte <dany_laporte@hotmail.com>
2015-05-07 20:37:03 -0400

    add stats to test runner

--- src/testRunner.js
+++ src/testRunner.js
@@ -1,32 +1,44 @@
+var Stats = require('fast-stats').Stats;
+
 function TestRunner(test, options, cb) {
     this.cb = cb;
     this.done = this.done.bind(this);
     this.duration = 0;
     this.execute = this.execute.bind(this);
     this.func = test.func;
-    this.options = options;
     this.name = test.name;
-    this.samples = [];
+    this.options = {
+        maxDuration: options.maxDuration || 1000,
+        maxSample: options.maxSample || 1000
+    };
     this.start = null;
+    this.stats = new Stats({ sampling: true, store_data: false });
 }
 
 TestRunner.prototype = {
     done: function () {
         var v =  process.hrtime(this.start)[1]/1000000; // nano to ms
-        this.samples.push(v);
+        this.stats.push(v);
         this.duration += v;
         process.nextTick(this.execute);
     },
     execute: function () {
-        if ((this.options.maxSample && this.samples.length < this.options.maxSample) &&
-            this.duration < options.maxDuration) {
+        if ((this.options.maxSample && this.stats.length < this.options.maxSample) &&
+            this.duration < this.options.maxDuration) {
             this.start = process.hrtime();
             this.func(this.done);
         } else {
+            var avg = this.stats.amean();
+            var r = this.stats.range();
+            
             this.cb({
+                avg: Math.round(avg*100)/100,
+                duration: Math.round(this.duration*100)/100,
+                errorMargin: Math.round((this.stats.moe()/avg)*10000)/100,
                 name: this.name,
-                duration: this.duration,
-                samples: samples
+                max: Math.round(r[1]*100)/100,
+                min: Math.round(r[0]*100)/100,
+                sd: Math.round(this.stats.σ()*100)/100
             });
         }
     }
